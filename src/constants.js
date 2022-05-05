6519ce150ce5acb55f2281f4e6523440c166f656
RyuKiyo66 <ryutaro.kiyohara@gmail.com>
2022-05-05 17:36:01 +0900

    upload to github

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+// CONTRACT_ADDRESSに、コントラクトアドレスを保存します。
+const CONTRACT_ADDRESS = '0x88E81C5aAC9C2d45e8A909AFb2fA1c7967F8d618';
+
+//NFT キャラクター の属性をフォーマットしてオブジェクトとして返します。
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+// 変数を constants.js 以外の場所でも使えるようにします。
+export { CONTRACT_ADDRESS, transformCharacterData };
